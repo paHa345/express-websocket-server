c4f7369b5f71e69bea3aa65a5a9acf08de318bcb
paHa345 <pav.345@mail.ru>
2025-01-28 11:35:52 +0700

    add websocket v1

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,34 @@
 const express = require("express");
+const WS = require("ws");
 
 const app = express();
+const server = require("http").createServer(app);
+const wss = new WS.Server({ server });
+
+app.use(express.json());
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/ws", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end();
+});
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+
+  // Handle incoming messages
+  ws.on("message", (message) => {
+    console.log(`Received message: ${message}`);
+    // Process the message and send a response
+    ws.send(`Server response: ${message}`);
+  });
+
+  // Handle disconnections
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+server.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
